8b90b0f35c0cf391f5e168be213541d08aa08435
Dylan <viad090723hmclvya2@cbtis217.edu.mx>
2025-12-01 18:03:08 -0600

    Segunda version de Dylan

---
+++ logueo/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: CC2_PC34
+ * Fecha: 01/12/2025
+ * Hora: 05:59 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace logueo
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblUsuario = new System.Windows.Forms.Label();
+			this.lblContraseña = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.txtUsuario = new System.Windows.Forms.TextBox();
+			this.txtContraseña = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// lblUsuario
+			// 
+			this.lblUsuario.Location = new System.Drawing.Point(13, 13);
+			this.lblUsuario.Name = "lblUsuario";
+			this.lblUsuario.Size = new System.Drawing.Size(100, 23);
+			this.lblUsuario.TabIndex = 0;
+			this.lblUsuario.Text = "Usuario:";
+			// 
+			// lblContraseña
+			// 
+			this.lblContraseña.Location = new System.Drawing.Point(13, 65);
+			this.lblContraseña.Name = "lblContraseña";
+			this.lblContraseña.Size = new System.Drawing.Size(100, 23);
+			this.lblContraseña.TabIndex = 1;
+			this.lblContraseña.Text = "Contraseña:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(78, 126);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Entrar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// txtUsuario
+			// 
+			this.txtUsuario.Location = new System.Drawing.Point(119, 13);
+			this.txtUsuario.Name = "txtUsuario";
+			this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+			this.txtUsuario.TabIndex = 3;
+			// 
+			// txtContraseña
+			// 
+			this.txtContraseña.Location = new System.Drawing.Point(120, 65);
+			this.txtContraseña.Name = "txtContraseña";
+			this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+			this.txtContraseña.TabIndex = 4;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.txtContraseña);
+			this.Controls.Add(this.txtUsuario);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lblContraseña);
+			this.Controls.Add(this.lblUsuario);
+			this.Name = "MainForm";
+			this.Text = "logueo";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox txtContraseña;
+		private System.Windows.Forms.TextBox txtUsuario;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label lblContraseña;
+		private System.Windows.Forms.Label lblUsuario;
+	}
+}
